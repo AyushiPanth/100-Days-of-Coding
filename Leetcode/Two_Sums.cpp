6f2ef98320ac6d66b74d60249d60b5a7e3fe6de6
Ayushi Panth <54657980+AyushiPanth@users.noreply.github.com>
2020-11-03 13:45:36 +0530

    Create Two_Sums.cpp

---
+++ Leetcode/Two_Sums.cpp
@@ -0,0 +1,57 @@
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 105
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists. */
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> numbers;
+        map<int, int> n;
+        for(int i = 0; i < nums.size(); i++) {
+            n[nums[i]] = i;
+        }
+        map<int, int>::iterator itr;
+        map<int, int>::iterator itr2;
+        for(int i = 0; i < nums.size(); i++) {
+            itr = n.find(target - nums[i]);
+            if(itr != n.end()) {
+                if(i == itr->second)
+                    continue;
+                else {
+                    numbers.push_back(i);
+                    numbers.push_back(itr->second);
+                    break;
+                }
+            }
+        }
+        
+    return numbers;
+    }
+};
